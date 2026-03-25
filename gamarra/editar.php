85e5ba40324ae1efe240b5fd8827d6e06856ea72
GGgamigus <gustavo.14050@aluno.iffar.edu.br>
2026-03-25 16:18:01 -0300

    Update editar.php

--- gamarra/editar.php
+++ gamarra/editar.php
@@ -1,10 +1,9 @@
 <?php
 include('conexao.php');
 
-$id = $_GET['id'];
+$id = intval($_GET['id']);
 
 $sql = "SELECT * FROM contatos WHERE id = $id";
-
 $resultado = mysqli_query($conexao, $sql);
 
 if (mysqli_num_rows($resultado) == 1) {
@@ -15,53 +14,54 @@ if (mysqli_num_rows($resultado) == 1) {
 }
 
 if (isset($_POST['atualizar'])) {
-
     $novo_nome = $_POST['nome'];
     $novo_endereco = $_POST['endereco'];
     $novo_fone = $_POST['fone'];
 
-    $sql2 = "UPDATE contatos SET nome='$novo_nome', endereco='$novo_endereco', 
-    telefone='$novo_fone' WHERE id = $id";
+    $sql_update = "UPDATE contatos 
+    SET nome='$novo_nome', endereco='$novo_endereco', telefone='$novo_fone'
+    WHERE id = $id";
 
-    if (mysqli_query($conexao, $sql2)) {
-        echo "<h2>Contato foi atualizado com sucesso!</h2>";
-        echo "<a href='index.php'>VOLTAR</a>";
-        exit;
+    if (mysqli_query($conexao, $sql_update)) {
+
+        echo "<!DOCTYPE html>
+        <html lang='pt-br'>
+        <head>
+            <meta charset='UTF-8'>
+            <link rel='stylesheet' href='agenda.css'>
+            <title>Sucesso</title>
+        </head>
+        <body class='body-mensagem'>
+            <div class='container-mensagem'>
+                <h2>Contato atualizado com sucesso!</h2>
+                <a href='index.php' class='btn-voltar'>VOLTAR PARA A AGENDA</a>
+            </div>
+        </body>
+        </html>";
+        exit; 
     } else {
-        echo "<h2>Erro ao atulizar contato. " . mysqli_error($conexao);
-        echo "<a href='index.php'>VOLTAR</a>";
+        echo "Erro ao atualizar: " . mysqli_error($conexao);
         exit;
     }
 }
-
-
-
-
 ?>
 
-<h1>Edição de contatos</h1>
-<form method="POST">
-    Nome: <input type="text" name="nome" value="<?php echo $contato['nome']; ?>"><br><br>
-    Endereço: <input type="text" name="endereco" value="<?php echo $contato['endereco']; ?>"><br><br>
-    Telefone: <input type="text" name="fone" value="<?php echo $contato['telefone']; ?>">
-    <br><br>
-    
-    <input type="submit" name="atualizar" value="Atualizar">
-    
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Edição de Contatos</title>
     <link rel="stylesheet" href="agenda.css">
-  
-
-</form>
-
-<?php
-
-if (mysqli_query($conexao, $sql2)) {
-    echo "<div class='container'>";
-    echo "<h2>Contato atualizado com sucesso!</h2>";
-    echo "<a href='index.php' class='btn-voltar'>Voltar para a Agenda</a>";
-    echo "</div>";
-    exit; 
-}
-
-?>
-
+</head>
+<body>
+    <h1>Edição de contatos</h1>
+    <form method="POST">
+        Nome: <input type="text" name="nome" value="<?php echo $contato['nome']; ?>"><br><br>
+        Endereço: <input type="text" name="endereco" value="<?php echo $contato['endereco']; ?>"><br><br>
+        Telefone: <input type="text" name="fone" value="<?php echo $contato['telefone']; ?>">
+        <br><br>
+        <input type="submit" name="atualizar" value="Atualizar" class="btn-cadastrar">
+    </form>
+    <a href='index.php' class='btn-voltar'>VOLTAR PARA A AGENDA</a>
+</body>
+</html>
