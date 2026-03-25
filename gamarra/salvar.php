7b54bd5219b9151599a67c5c3fd1b4b7bbe3b022
GGgamigus <gustavo.14050@aluno.iffar.edu.br>
2026-03-25 16:19:31 -0300

    Update salvar.php

--- gamarra/salvar.php
+++ gamarra/salvar.php
@@ -1,5 +1,4 @@
 <?php
-
 include('conexao.php');
 
 $nome = $_POST['nome'];
@@ -7,12 +6,19 @@ $endereco = $_POST['endereco'];
 $fone = $_POST['fone'];
 
 $sql = "INSERT INTO contatos (nome, endereco, telefone) 
-         VALUES ('$nome', '$endereco', '$fone')";
+        VALUES ('$nome', '$endereco', '$fone')";
+
+
+echo "<link rel='stylesheet' href='agenda.css'>";
+echo "<div class='container-mensagem'>"; 
 
 if (mysqli_query($conexao, $sql)) {
     echo "<h2>Cadastro realizado com sucesso!</h2>";
-    echo "<a href='index.php'>VOLTAR</a>";
 } else {
-    echo "<h2>Erro ao salvar o contato!</h2>" . mysqli_error($conexao);
-    echo "<a href='index.php'>VOLTAR</a>";
+    echo "<h2>Erro ao salvar contato</h2>";
+    echo "<p>" . mysqli_error($conexao) . "</p>";
 }
+
+echo "<a href='index.php' class='btn-voltar'>VOLTAR</a>";
+echo "</div>";
+?>
